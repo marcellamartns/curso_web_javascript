9bbbf73e56c0da70e4bf93920432d33dada43566
martins-marcella@hotmail.com <martins-marcella@hotmail.com>
2019-10-22 16:55:46 -0300

    ok

---
+++ arrays.js
@@ -0,0 +1,17 @@
+const valores = [1, 3, 6, 8]
+
+
+console.log(valores[2])
+
+console.log(valores.length)
+
+valores.push("teste", 12, 234, {id:5})
+
+console.log(valores)
+
+console.log(valores.pop())
+
+delete valores[1]
+
+console.log(typeof valores)
+
